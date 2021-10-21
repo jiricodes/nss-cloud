7f037143a88f7bf442932df1e640d990be57b346
Tuomas Junno <tuomas.junno@cs.helsinki.fi>
2021-10-21 11:06:54 +0300

    update

--- conf/ldap.sh
+++ conf/ldap.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-docker run -p 389:389 -p 636:636 --name ldap --detach --memory=384m --memory-reservation=256m --memory-swap=768m --cpus=.5 --restart always osixia/openldap:1.5.0
+docker run -p 389:389 -p 636:636 --name ldap --detach --memory=384m --memory-reservation=256m --memory-swap=768m --cpus=.5 --restart always \
+-v /home/cloud-user/certs:/container/service/slapd/assets/certs \
+-e LDAP_TLS_CA_CRT_FILENAME=ca.crt \
+-e LDAP_TLS_CRT_FILENAME=cert1.pem \
+-e LDAP_TLS_KEY_FILENAME=ldap.key \
+-e ALLOW_EMPTY_PASSWORD=yes \
+-e LDAP_TLS_VERIFY_CLIENT=try \
+osixia/openldap:1.5.0 --loglevel=trace
